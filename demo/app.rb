d4d970562f691547aff15035b18cf32a9db3c2b4
David Rasch <rasch@raschnet.com>
2013-01-28 20:44:58 -0500

    sinatra working now

--- demo/app.rb
+++ demo/app.rb
@@ -1,9 +1,20 @@
+require 'rubygems'
 require 'sinatra'
 require 'mysql2'
-client = Mysql2::Client.new(:database => "test")
+
 
 get '/' do
-  client.query("select sql_no_cache  code, count(*) as ct from wikimedia_hits group by 1 order by 2 desc limit 10")
+  @client ||= Mysql2::Client.new(:database => "test")
+
+  @data = @client.query("select code, count(*) as ct from wikimedia_hits group by 1 order by 2 desc limit 10")
+  #@client.query("update wikimedia_hits set hits = hits+1 where id = 1")
+
+  erb :'data'
 end
 
+get '/hit' do
+  @client ||= Mysql2::Client.new(:database => "test")
+
+  @client.query("update wikimedia_hits set hits= hits +1 -1 where id = 1")
+end
 
