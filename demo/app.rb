4a50ad8b12cc80517d5b732f404920a9a0c0748b
David Rasch <rasch@raschnet.com>
2013-01-28 21:08:45 -0500

    self setup

--- demo/app.rb
+++ demo/app.rb
@@ -9,8 +9,24 @@ get '/' do
   @data = @client.query("select code, count(*) as ct from wikimedia_hits group by 1 order by 2 desc limit 10")
   #@client.query("update wikimedia_hits set hits = hits+1 where id = 1")
 
-  erb :'data'
+  erb :data
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 get '/hit' do
   @client ||= Mysql2::Client.new(:database => "test")
--- demo/getdata.sh
+++ demo/getdata.sh
@@ -1,3 +1,1 @@
-wget http://dumps.wikimedia.org/other/pagecounts-raw/2013/2013-01/pagecounts-20130101-000000.gz
-
-zcat pagecounts-20130101-000000.gz > /usr/shrae/mysql/wikimedia_hits.ssv
+curl -# http://dumps.wikimedia.org/other/pagecounts-raw/2013/2013-01/pagecounts-20130101-000000.gz | gzip -d - > /usr/share/mysql/wikimedia_hits.ssv
